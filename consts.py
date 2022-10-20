76f782cc7ea74a977705fee2dc73b099a0e56500
om1ji <om1ji.music@gmail.com>
2022-10-20 17:23:16 +0300

    Update consts.py

--- consts.py
+++ consts.py
@@ -1,4 +1,4 @@
 ADMINS = [] # Telegram User ID
 TOKEN = "" # Bot token
-RADIOBOSS_SERVER = "" # URL of your server which runs RadioBOSS
+RADIOBOSS_SERVER = "" # URL and port of your server where RadioBOSS is running (If your bot is on the same machine as RadioBOSS, localhost can be passed)
 RADIOBOSS_PASSWORD = "" # Password of RadioBOSS API access (More: https://manual.djsoft.net/radioboss/en/index.html?remote_controlapi.htm)
